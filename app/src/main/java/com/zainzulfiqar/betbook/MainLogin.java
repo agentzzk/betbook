620360c8f093ae2d77f1ae1af5a9ef9429246480
Zain Zulfiqar <contact@zainzulfiqar.com>
2016-08-13 11:53:34 -0400

    Create a simple Hello World application
    Use external Java class to generate data

--- app/src/main/java/com/zainzulfiqar/betbook/MainLogin.java
+++ app/src/main/java/com/zainzulfiqar/betbook/MainLogin.java
@@ -2,6 +2,10 @@ package com.zainzulfiqar.betbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    protected void getDays(View view) {
+        Spinner month = (Spinner) findViewById(R.id.months);
+        daysOfMonths sample = new daysOfMonths();
+        int days = sample.days(String.valueOf(month.getSelectedItem()));
+        TextView text = (TextView) findViewById(R.id.displayDays);
+        text.setText("This month has " + days + " days.");
     }
 }
