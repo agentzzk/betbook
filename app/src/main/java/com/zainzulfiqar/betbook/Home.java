bd056f22fae554391af47bc7facfa77fdb3576ed
Zain Zulfiqar <contact@zainzulfiqar.com>
2016-08-20 23:17:35 -0400

    Setup ListActivity; Start building core

--- app/src/main/java/com/zainzulfiqar/betbook/Home.java
+++ app/src/main/java/com/zainzulfiqar/betbook/Home.java
@@ -1,37 +1,23 @@
 package com.zainzulfiqar.betbook;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        AdapterView.OnItemClickListener betsListener = new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0){
-                    Toast.makeText(getApplicationContext(), "first", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "other", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-        ListView bets = (ListView) findViewById(R.id.listBets);
-        bets.setOnItemClickListener(betsListener);
-    }
-
-    protected void createBet(View view) {
-        Intent createBet = new Intent(this, CreateBet.class);
-        startActivityForResult(createBet, 1);
+        ListView listBets = getListView();
+        String[] test = {"hello", "testing", "123"};
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, test);
+        listBets.setAdapter(listAdapter);
     }
 }
